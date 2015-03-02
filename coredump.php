670bac292722be6b706d29bb1116296d89a2eb60
micrud <mickey>
2015-03-02 12:56:07 +0100

    testerino

---
+++ coredump.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html><head>
+<meta http-equiv="content-type" content="text/html; charset=windows-1252">
+        <link href="css/default.css" rel="stylesheet">
+    </head>
+
+    <body>
+        <div id="header">
+            test header
+        </div>
+
+        <div id="menu">
+            test menu
+        </div>
+
+        <div id="content">
+            <div id="sidebar">
+                test sidebar
+            </div>
+
+            <div id="shop">
+                test shop
+            </div>
+        </div>
+
+        <div id="footer">
+            test footer
+        </div>
+    
+</body></html>

e8de463791bf840e2360b09c994f1e7796ad8f72
mickey <mickeyberg401@gmail.com>
2015-03-16 11:30:18 +0100

    test maandag

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link href="css/default.css" rel="stylesheet">
+    </head>
+
+    <body>
+        <div id="header">
+            <img src="img/header.jpg">
+        </div>
+
+        <div id="menu">
+            test menu
+        </div>
+
+        <div id="content">
+            <div id="sidebar">
+                Testboudel
+            </div>
+
+            <div id="shop">
+                test shop
+            </div>
+        </div>
+
+        <div id="footer">
+            test footer
+        </div>
+    </body>
+</html>

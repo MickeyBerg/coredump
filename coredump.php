437fac45d9fec0a1ed217100a0a091e8e2eba7aa
xr6reactions <wouter.stulp@gmail.com>
2015-03-20 12:47:42 +0100

    Changes to be committed: 	modified:   coredump.php 	modified:   css/default.css
     Changes not staged for commit:
    	modified:   .idea/workspace.xml

--- coredump.php
+++ coredump.php
@@ -1,6 +1,12 @@
 <!DOCTYPE html>
 <html>
     <head>
+        <meta charset='utf-8'>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="styles.css">
+        <script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
+        <script src="script.js"></script>
         <link href="css/default.css" rel="stylesheet">
     </head>
 
@@ -9,15 +15,13 @@
             <img src="img/header.jpg">
         </div>
         <div id="content">
-            <div id="menu-content">
-                    <ul>
-                        <li><a href="">home</a></li>
-                        <li><a href="">store</a></li>
-                        <li><a href="">info</a></li>
-                    </ul>
-                    <div id="userbar">
-                        <img src="img/winkel.jpg">
-                    </div>
+            <div id='cssmenu'>
+                <ul>
+                    <li class='active'><a href='#'><span>Home</span></a></li>
+                    <li><a href='#'><span>Products</span></a></li>
+                    <li><a href='#'><span>Company</span></a></li>
+                    <li class='last'><a href='#'><span>Contact</span></a></li>
+                </ul>
             </div>
             <div id="sidebar">
                 Testboudel

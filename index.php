36daa4416e3f788579119ce8e178811ed5f20d14
xr6reactions <wouter.stulp@gmail.com>
2015-03-20 13:40:52 +0100

    shop layout

--- index.php
+++ index.php
@@ -1,6 +1,12 @@
 <!DOCTYPE html>
 <html>
     <head>
+        <meta charset='utf-8'>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="styles.css">
+        <script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
+        <script src="script.js"></script>
         <link href="css/default.css" rel="stylesheet">
     </head>
 
@@ -8,18 +14,47 @@
         <div id="header">
             <img src="img/header.jpg">
         </div>
-
-        <div id="menu">
-            test menu
-        </div>
-
         <div id="content">
+            <div id='cssmenu'>
+                <ul>
+                    <li class='active'><a href='#'><span>Home</span></a></li>
+                    <li><a href='#'><span>Products</span></a></li>
+                    <li><a href='#'><span>Company</span></a></li>
+                    <li class='last'><a href='#'><span>Contact</span></a></li>
+                </ul>
+            </div>
             <div id="sidebar">
                 Testboudel
             </div>
 
             <div id="shop">
-                test shop
+                <div class="cat_buttons">
+                    <img src="img/moederboard.jpg">
+                </div>
+                <div class="cat_buttons">
+                    <img src="img/moederboard.jpg">
+                </div>
+                <div class="cat_buttons">
+                    <img src="img/moederboard.jpg">
+                </div>
+                <div class="cat_buttons">
+                    <img src="img/moederboard.jpg">
+                </div>
+                <div class="cat_buttons">
+                    <img src="img/moederboard.jpg">
+                </div>
+                <div class="cat_buttons">
+                    <img src="img/moederboard.jpg">
+                </div>
+                <div class="cat_buttons">
+                    <img src="img/moederboard.jpg">
+                </div>
+                <div class="cat_buttons">
+                    <img src="img/moederboard.jpg">
+                </div>
+                <div class="cat_buttons">
+                    <img src="img/moederboard.jpg">
+                </div>
             </div>
         </div>
 

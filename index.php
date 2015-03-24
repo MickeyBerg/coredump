509e307d9e1a19226ff2d2b082cbb5b8c085490f
xr6reactions <wouter.stulp@gmail.com>
2015-03-24 13:31:20 +0100

    Added images

--- index.php
+++ index.php
@@ -4,7 +4,6 @@
         <meta charset='utf-8'>
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="styles.css">
         <script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
         <script src="script.js"></script>
         <link href="css/default.css" rel="stylesheet">
@@ -21,8 +20,15 @@
                     <li><a href='#'><span>Products</span></a></li>
                     <li><a href='#'><span>Company</span></a></li>
                     <li class='last'><a href='#'><span>Contact</span></a></li>
+                    <img src="img/winkel.jpg" style; width="50px"; height="50px">
                 </ul>
             </div>
+            <?
+
+            include 'menu.php';
+
+            ?>
+
             <div id="sidebar">
                 <a href="#"><p>Moederborden</p></a>
                 <a href="#"><p>SSD's</p></a>
@@ -40,13 +46,16 @@
                         <p>Moederborden</p>
                 </div></a>
                 <div class="cat_buttons">
-                    <img src="img/moederboard.jpg">
+                    <img src="img/ssd.jpg">
+                    <p>SSD's</p>
                 </div>
                 <div class="cat_buttons">
-                    <img src="img/moederboard.jpg">
+                    <img src="img/processor.jpg">
+                    <p>Processoren</p>
                 </div>
                 <div class="cat_buttons">
-                    <img src="img/moederboard.jpg">
+                    <img src="img/videokaart.jpg">
+                    <p>Videokaarten</p>
                 </div>
                 <div class="cat_buttons">
                     <img src="img/moederboard.jpg">

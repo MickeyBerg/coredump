f6e05c83f3942978b65c0c78a41ca5137a388a4c
xr6reactions <wouter.stulp@gmail.com>
2015-03-26 09:29:53 +0100

    Login CSS gemaakt

--- index.php
+++ index.php
@@ -8,6 +8,7 @@
         <script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
         <script src="script.js"></script>
         <link href="css/default.css" rel="stylesheet">
+        <script src="login.js"></script>
     </head>
 
     <body>
@@ -23,12 +24,30 @@
                     <li class='last'><a href='#'><span>Contact</span></a></li>
                 </ul>
             </div>
-
-            <?
-
-            include 'menu.php';
-
-            ?>
+            <nav>
+                <ul>
+                    <li id="login">
+                        <a id="login-trigger" href="#">
+                            Log in <span>▼</span>
+                        </a>
+                        <div id="login-content">
+                            <form>
+                                <fieldset id="inputs">
+                                    <input id="username" type="email" name="Email" placeholder="Your email address" required>
+                                    <input id="password" type="password" name="Password" placeholder="Password" required>
+                                </fieldset>
+                                <fieldset id="actions">
+                                    <input type="submit" id="submit" value="Log in">
+                                    <label><input type="checkbox" checked="checked"> Keep me signed in</label>
+                                </fieldset>
+                            </form>
+                        </div>
+                    </li>
+                    <li id="signup">
+                        <a href="">Sign up FREE</a>
+                    </li>
+                </ul>
+            </nav>
 
             <div id="sidebar">
                 <a href="#"><p>Moederborden</p></a>
